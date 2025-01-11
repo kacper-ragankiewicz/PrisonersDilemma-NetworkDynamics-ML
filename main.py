3999b785e232af4cf225611798c03bc94a23c08d
Kacper Ragankiewicz <roko321@protonmail.com>
2025-01-11 16:09:46 +0100

    adding social network

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ def prisoner_dilemma(strategy1, strategy2, rounds=100):
     tournament = axl.Tournament([strategy1(), strategy2()], turns=rounds)
     results = tournament.play()
     score1, score2 = results.scores[0][0], results.scores[1][0]
+
     return score1, score2
 
 
 # Example usage
 score1, score2 = prisoner_dilemma(axl.TitForTat, axl.Defector)
+
 print(f"Tit for Tat vs Always Defect: {score1} - {score2}")
--- tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-
-import argparse
-import os
-
-import numpy
-import matplotlib.pyplot as plt
-
-import axelrod
